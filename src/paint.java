5b2449b11b60580fc9084ac9c33731be6a8026e2
rachelpatek <rpatek4@gmail.com>
2019-06-06 13:29:46 -0400

    its works

--- src/paint.java
+++ src/paint.java
@@ -4,14 +4,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.BorderPane;
+
 
 public class paint extends Application{
     private Pane pane = new Pane();
     private Button btSize = new Button("Enter");
     private ComboBox dropSize = new ComboBox();
+    private ComboBox dropColor = new ComboBox();
     private HBox thiccRow = new HBox();
     private BorderPane brdpane = new BorderPane();
     @Override
@@ -21,6 +24,12 @@ public class paint extends Application{
         brdpane.setTop(thiccRow);
         brdpane.setCenter(pane);
 
+        btSize.setStyle("-fx-font-size:15 ");
+
+        pane.setOnMouseDragged(e->{
+            Rectangle draw = new Rectangle(e.getX(),e.getY(),3,3);
+            pane.getChildren().add(draw);
+        });
 
         Scene scene = new Scene(brdpane,1000, 1000);
         primaryStage.setTitle("test2");
