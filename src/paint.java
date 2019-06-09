7d8648028db357f27bbc19bb3f0467cc73f4192c
Emma Duda <emmdud21@bergen.org>
2019-06-09 12:25:34 -0400

    added color, brush size, and new canvas

--- src/paint.java
+++ src/paint.java
@@ -1,40 +1,132 @@
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.event.*;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.Pane;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.BorderPane;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+
+import javax.swing.*;
+import java.awt.*;
 
 
 public class paint extends Application{
     private Pane pane = new Pane();
-    private Button btSize = new Button("Enter");
-    private ComboBox dropSize = new ComboBox();
+    private Button btn = new Button("New Canvas");
+    private ComboBox comboBox = new ComboBox();
     private ComboBox dropColor = new ComboBox();
     private HBox thiccRow = new HBox();
     private BorderPane brdpane = new BorderPane();
+    private Color currentColor = Color.BLACK;
+    //private boolean erase = false;
+    private double currentBrushSize =10 ;
+    private Slider slider = new Slider();
+
+
     @Override
     public void start (Stage primaryStage){
-        thiccRow.getChildren().add(dropSize);
-        thiccRow.getChildren().add(btSize);
+        comboBox.getItems().addAll(
+                "Black",
+                "Blue",
+                "Green",
+                "Red",
+                "Yellow"
+        );
+        slider.setMin(1);
+        slider.setMax(35);
+        slider.setValue(currentBrushSize);
+
+        thiccRow.getChildren().add(comboBox);
+
+
+        thiccRow.getChildren().add(slider);
+        thiccRow.getChildren().add(btn);
         brdpane.setTop(thiccRow);
         brdpane.setCenter(pane);
 
-        btSize.setStyle("-fx-font-size:16 ");
+
+
+        btn.setStyle("-fx-font-size:16 ");
 
         pane.setOnMouseDragged(e->{
-            Rectangle draw = new Rectangle(e.getX(),e.getY(),3,3);
+            Rectangle draw = new Rectangle(e.getX(),e.getY(),currentBrushSize,currentBrushSize);
+            draw.setFill(currentColor);
+
+//            if(erase){
+//                pane.getChildren().add(draw);
+//
+//            }
+//            else {
+//                pane.getChildren().add(draw);
+//            }
             pane.getChildren().add(draw);
+
+
         });
 
+
+        comboBox.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String item = comboBox.getValue().toString();
+                Color drawColor = getColor(item);
+                currentColor = drawColor;
+            }
+        });
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                restart();
+            }
+        });
+
+
+        slider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                                Number old_val, Number new_val) {
+                System.out.println(new_val.doubleValue());
+                currentBrushSize = new_val.doubleValue();
+            }
+        });
+
+
+
+
         Scene scene = new Scene(brdpane,1000, 1000);
-        primaryStage.setTitle("test2");
+        primaryStage.setTitle("Paint Application");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
+    }
+
+    public Color getColor(String colorName){
+        if(colorName.equals("Red"))
+            return Color.RED;
+        else if(colorName.equals("Blue"))
+            return Color.BLUE;
+        else if(colorName.equals("Black"))
+            return Color.BLACK;
+        else if(colorName.equals("Green"))
+            return Color.GREEN;
+        else {
+            return Color.YELLOW;
+        }
+    }
+
+    public void restart(){
+        pane.getChildren().clear();
     }
 
 }
